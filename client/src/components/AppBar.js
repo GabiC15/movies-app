98497787d2c5f06c250c0f1ef784cbbf43065b4c
Gabriel <luisgabrielcasares@gmail.com>
2023-04-29 17:58:01 -0300

    fix: remove appbar icons

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -8,7 +8,6 @@ import Menu from "@mui/material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import MoreIcon from "@mui/icons-material/MoreVert";
 import { useState } from "react";
 import { Container } from "@mui/system";
 import { Divider } from "@mui/material";
@@ -33,10 +32,6 @@ export default function CommonAppBar() {
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
   };
 
   const menuId = "primary-search-account-menu";
@@ -120,61 +115,23 @@ export default function CommonAppBar() {
         sx={{ backgroundColor: "#ffffff", boxShadow: "none" }}
       >
         <Container
-          sx={{ display: "flex", alignItems: "center", padding: ".5rem 0" }}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            padding: ".5rem 0",
+          }}
         >
           <Box onClick={() => navigate("/")}>
             <img
               src={logo}
               alt="Logo"
               style={{
+                marginTop: ".3rem",
                 height: "2rem",
               }}
             />
           </Box>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="#000000"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="#000000"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="#000000"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
         </Container>
       </AppBar>
       <Divider />
